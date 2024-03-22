a821df79fa40d36a2afc18df1b04827db386eded
Wilfred Mutwiri <Wilfred Mutwiri>
2024-03-22 13:22:44 +0300

    Add about,contacts pages?

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import Flask,render_template
 app=Flask(__name__)
 @app.route('/')
+def index1():
+    return render_template("index.html")
+#index route
+@app.route('/index.html')
 def index():
     return render_template("index.html")
+#home route
+@app.route('/home.html')
+def home():
+    return render_template("home.html")
+#about route
+@app.route('/about.html')
+def about():
+    return render_template("about.html")
+#contacts route
+@app.route('/contact.html')
+def contact():
+    return render_template("contact.html")
 if __name__=="__main__":
     app.run(debug=True)
